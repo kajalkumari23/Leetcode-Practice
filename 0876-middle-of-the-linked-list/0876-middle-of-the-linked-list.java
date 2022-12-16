9e296ba16dad5a890f3fd9581b2d50bbfb95429e
Kajal Kumari <kajalkumari36240@gmail.com>
2022-12-17 01:31:29 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (90.42%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -21,14 +21,3 @@ class Solution {
     }
 }
 
-// class Solution {
-//     public ListNode middleNode(ListNode head) {
-//         ListNode slow = head, fast = head;
-// 		// slow moving 1 step at a time while fast is moving 2 steps at a time #O(N/2)
-//         while (fast != null && fast.next != null) {
-//             slow = slow.next;
-//             fast = fast.next.next;
-//         }
-//         return slow;
-//     }
-// }
