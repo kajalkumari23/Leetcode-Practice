a1cf794427a826500b2d3dc01f23be44208b5037
Kajal Kumari <kajalkumari36240@gmail.com>
2023-01-08 16:47:09 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (61.56%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,17 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        HashSet<Integer> hashset = new HashSet<>();
-        
-        for(int i=0; i<n; i++){
-            hashset.add(nums[i]);
+        int total = n*(n+1)/2;
+        int sum =0;
+        for(int num:nums){
+            sum += num;
         }
-        for(int i=0; i<n; i++){
-            if(!hashset.contains(i)){
-                return i;
-            }
-        }
-        return n;
+        return total-sum;
     }
 }
-
